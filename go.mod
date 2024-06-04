21f6e212cbc52f186dc5c66149659bcd7cfcb130
Tobias Persson <tobiaspn@axis.com>
2024-06-04 11:38:34 +0200

    Add a new API for listing logs and artifacts from a log area

--- go.mod
+++ go.mod
@@ -1,18 +1,25 @@
 module github.com/eiffel-community/etos-api
 
-go 1.20
+go 1.21
+
+toolchain go1.22.1
 
 require (
+	github.com/fernet/fernet-go v0.0.0-20240119011108-303da6aec611
+	github.com/jmespath/go-jmespath v0.4.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/snowzach/rotatefilehook v0.0.0-20220211133110-53752135082d
-	github.com/stretchr/testify v1.8.2
+	github.com/stretchr/testify v1.8.4
 	go.elastic.co/ecslogrus v1.0.0
+	go.etcd.io/etcd/client/v3 v3.5.14
 	k8s.io/apimachinery v0.28.2
 	k8s.io/client-go v0.28.2
 )
 
 require (
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
@@ -20,11 +27,11 @@ require (
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magefile/mage v1.9.0 // indirect
@@ -40,6 +47,22 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.14 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.14 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.11.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
---
+++ pkg/logarea/v1alpha/logarea.go
@@ -0,0 +1,179 @@
+// Copyright Axis Communications AB.
+//
+// For a full list of individual contributors, please see the commit history.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package logarea
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"time"
+
+	"github.com/eiffel-community/etos-api/internal/config"
+	"github.com/eiffel-community/etos-api/pkg/application"
+	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+// REGEX for matching /testrun/tercc-id/suite/main-suite-id/subsuite/subsuite-id/suite.
+const REGEX = "/testrun/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/suite/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/subsuite/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/suite"
+
+type LogAreaApplication struct {
+	logger *logrus.Entry
+	cfg    config.Config
+	client *clientv3.Client
+	regex  *regexp.Regexp
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+type LogAreaHandler struct {
+	logger *logrus.Entry
+	cfg    config.Config
+	ctx    context.Context
+	client *clientv3.Client
+	regex  *regexp.Regexp
+}
+
+// Close cancels the application context and closes the ETCD client.
+func (a *LogAreaApplication) Close() {
+	a.client.Close()
+	a.cancel()
+}
+
+// New returns a new LogAreaApplication object/struct.
+func New(cfg config.Config, log *logrus.Entry, ctx context.Context) application.Application {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create etcd client")
+		return nil
+	}
+	// MustCompile panics if the regular expression cannot be compiled.
+	// Since the regular expression is hard-coded, it should never fail in production.
+	regex := regexp.MustCompile(REGEX)
+	ctx, cancel := context.WithCancel(ctx)
+	return &LogAreaApplication{
+		logger: log,
+		cfg:    cfg,
+		client: cli,
+		regex:  regex,
+		ctx:    ctx,
+		cancel: cancel,
+	}
+}
+
+// LoadRoutes loads all the v1alpha routes.
+func (a LogAreaApplication) LoadRoutes(router *httprouter.Router) {
+	handler := &LogAreaHandler{a.logger, a.cfg, a.ctx, a.client, a.regex}
+	router.GET("/v1alpha/selftest/ping", handler.Selftest)
+	router.GET("/v1alpha/logarea/:identifier", handler.GetFileURLs)
+}
+
+// Selftest is a handler to just return 204.
+func (h LogAreaHandler) Selftest(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(http.StatusNoContent)
+}
+
+type Response map[string]Directory
+
+type Downloadable struct {
+	URL  string       `json:"url"`
+	Name []FilterType `json:"name"`
+}
+
+type Directory struct {
+	Logs      []Downloadable `json:"logs"`
+	Artifacts []Downloadable `json:"artifacts"`
+}
+
+// GetFileURLs is an endpoint for getting file URLs from a log area.
+func (h LogAreaHandler) GetFileURLs(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*15)
+	defer cancel()
+	directories := make(Response)
+	identifier := ps.ByName("identifier")
+	// Making it possible for us to correlate logs to a specific connection
+	logger := h.logger.WithField("identifier", identifier)
+
+	response, err := h.client.Get(ctx, fmt.Sprintf("/testrun/%s/suite", identifier), clientv3.WithPrefix())
+	if err != nil {
+		logger.WithError(err).Error("Failed to get file URLs")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Add("Retry-After", "10")
+		return
+	}
+	for _, ev := range response.Kvs {
+		// Verify that 'ev.Value' is an actual sub suite definition and not another
+		// field in the ETCD database. Since we are prefix searching on /testrun/suiteid/suite
+		// it is very possible that more data will arrive than we are interested in.
+		if !h.regex.Match(ev.Key) {
+			continue
+		}
+		suite := Suite{}
+		if err := json.Unmarshal(ev.Value, &suite); err != nil {
+			logger.WithError(err).Error("Failed to unmarshal suite")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Add("Retry-After", "10")
+			return
+		}
+		response, err := suite.LogArea.Download.Request.Do(ctx, logger)
+		if err != nil {
+			// HTTP error
+			logger.WithError(err).Error("Failed to download")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Add("Retry-After", "10")
+			return
+		}
+		defer response.Body.Close()
+		jsondata, err := io.ReadAll(response.Body)
+		if err != nil {
+			logger.WithError(err).Error("Failed to read response body")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Add("Retry-After", "10")
+			return
+		}
+		logUrls, err := suite.LogArea.Download.Filters.Logs.Run(jsondata, response.Header, ev.Value, suite.LogArea.Download.Filters.BaseURL)
+		if err != nil {
+			// HTTP error
+			logger.WithError(err).Error("Failed to download")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Add("Retry-After", "10")
+			return
+		}
+		artifactUrls, err := suite.LogArea.Download.Filters.Artifacts.Run(jsondata, response.Header, ev.Value, suite.LogArea.Download.Filters.BaseURL)
+		if err != nil {
+			// HTTP error
+			logger.WithError(err).Error("Failed to download")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Header().Add("Retry-After", "10")
+			return
+		}
+		directories[suite.Name] = Directory{Logs: logUrls, Artifacts: artifactUrls}
+	}
+
+	resp, _ := json.Marshal(directories) //nolint:errchkjson
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(resp)
+}
